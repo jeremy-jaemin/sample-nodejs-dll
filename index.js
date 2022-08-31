d5b6d7e0b229ff64c5dd8da3617873e53128eff8
jeremy-jaemin <kjm07252@gmail.com>
2022-08-31 17:13:16 +0900

    코드 클린

--- index.js
+++ index.js
@@ -1,32 +1,9 @@
 const ffi = require("ffi-napi");
-const path = require("path");
 
-// test 1
 const libfactorial = ffi.Library("./DllTest_x64.dll", {
   factorial: ["int", ["int"]],
 });
 
-// const arch = process.arch;
-// const libfactorial = ffi.Library(path.resolve(__dirname, `../${arch}`), {
-//   factorial: ["int", ["int"]],
-// });
-
-console.log("=============================================");
+console.log("4! = ");
 const result = libfactorial.factorial(4);
 console.log(result);
-console.log("=============================================");
-
-// // test 2
-// const libm = ffi.Library("libm", {
-//   ceil: ["double", ["double"]],
-// });
-// console.log("=============================================");
-// console.log(libm.ceil(1.5));
-// console.log("=============================================");
-
-// const libnull = ffi.Library(null, {
-//   atoi: ["int", ["string"]],
-// });
-// console.log("=============================================");
-// console.log(libnull.atoi("1234"));
-// console.log("=============================================");
